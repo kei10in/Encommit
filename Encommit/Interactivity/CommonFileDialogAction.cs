cf255bef1d80daf03fa6c9e4665020e0a61c6a82
kei10in <kei10in@gmail.com>
2016-03-26 11:38:30 +0900

    Implement Folder Open

---
+++ Encommit/Interactivity/CommonFileDialogAction.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interactivity;
+
+namespace Encommit.Interactivity
+{
+    public class FileDialogAction : TriggerAction<DependencyObject>
+    {
+        public string FilePath
+        {
+            get { return (string)GetValue(FilePathProperty); }
+            set { SetValue(FilePathProperty, value); }
+        }
+
+        public static readonly DependencyProperty FilePathProperty =
+            DependencyProperty.Register("FilePath", typeof(string), typeof(FileDialogAction), new PropertyMetadata(""));
+
+
+        public bool IsFolderPicker
+        {
+            get { return (bool)GetValue(IsFolderPickerProperty); }
+            set { SetValue(IsFolderPickerProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsFolderPickerProperty =
+            DependencyProperty.Register("IsFolderPicker", typeof(bool), typeof(FileDialogAction), new PropertyMetadata(false));
+
+        protected override void Invoke(object parameter)
+        {
+            var dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = IsFolderPicker;
+
+            var result = dialog.ShowDialog();
+            if (result == CommonFileDialogResult.Ok)
+            {
+                FilePath = dialog.FileName;
+            }
+        }
+    }
+}
---
+++ Encommit/ViewModels/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encommit.ViewModels
+{
+    public class MainViewModel : ReactiveObject
+    {
+        public string RepositoryPath
+        {
+            get; set;
+        }
+    }
+}
